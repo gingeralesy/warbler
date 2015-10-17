556b87367cebe4c9453a782825e2eb1b303c5bff
gingeralesy <gingeralesy@gmail.com>
2015-10-17 14:43:05 +0300

    Added application launching

--- warbler.lisp
+++ warbler.lisp
@@ -7,12 +7,43 @@
 (in-package #:org.gingeralesy.warbler)
 (named-readtables:in-readtable :qtools)
 
+;; Qt widgets here
+
+(defvar *window*)
+
+(define-widget main-window (QMainWindow)
+  ())
+
+(define-initializer (main-window init-window-system 100)
+  (unless (boundp '*window*)
+    (error "Tried to create main window without the proper context!"))
+  (when *window*
+    (error "A main window instance is already active!"))
+  (setf *window* main-window))
+
+(define-initializer (main-window setup)
+  (setf (q+:window-title main-window)
+        (format NIL "Warbler v~a" (asdf:component-version
+                                   (asdf:find-system :warbler))))
+  (q+:resize main-window 500 500))
+
+(define-subwidget (main-window name) (q+:make-qlineedit main-window)
+  (setf (q+:placeholder-text name) "This is a placeholder widget."))
+
+(define-subwidget (main-window layout) (q+:make-qhboxlayout main-window)
+  (q+:add-widget layout name))
+
 ;; Main
 
 (defun main ()
   "Starts up the Warbler program"
-  (with-main-window (window (make-instance 'main-window))))
-
-(define-widget main-window (QWidget)
-  ())
-
+  (qt:make-qapplication)
+  (format t " ** Warbler launching.~%")
+  (let* ((*window*)
+         (window (make-instance 'main-window)))
+    (q+:show window)
+    (format t " ** Warbler finished with ~a.~%"
+            (q+:exec *qapplication*))
+    (finalize window)
+    (trivial-garbage:gc :full T)
+    NIL))
