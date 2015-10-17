fa08592e14d34482bfb556357ceebcea3c272a67
gingeralesy <gingeralesy@gmail.com>
2015-10-17 13:14:53 +0300

    Project base

---
+++ warbler.asd
@@ -0,0 +1,8 @@
+
+(defsystem "warbler"
+  :name "Warbler"
+  :author "Janne"
+  :description "Weekly planner for projects and events."
+  :components ((:file "warbler"))
+  :depends-on (:qtools :qtcore :qtgui :q+))
+
---
+++ warbler.lisp
@@ -0,0 +1,14 @@
+
+(defpackage :warbler
+  (:documentation "UI for doing weekly planning.")
+  (:use #:cl+qt)
+  (:export #:main))
+
+(in-package :warbler)
+
+;; Main
+
+(defun main ()
+  "Starts up the Warbler program"
+  (print "Hello, World!"))
+
